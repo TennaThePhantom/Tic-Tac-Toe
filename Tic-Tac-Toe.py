434aceb038a1866b3724627dbecc451c98b160a3
Tennessee Foster <68438949+TennaThePhantom@users.noreply.github.com>
2021-10-05 14:56:58 -0400

    Day 1

---
+++ Tic-Tac-Toe.py
@@ -0,0 +1,9 @@
+# tic tak toe game
+# two player and vs ai mode
+# different board sizes 3x3 4x4 5x5 7x7x 10x10 20x20 30x30
+# with user interface
+# start menu another menu that shows the rules
+# menu ask for 2 player or ai and asks you want board size you want to play on 
+# can enter player 1 and player 2 name
+# X and O maybe other symbols like custom symbols for each player if it's two player
+# 
