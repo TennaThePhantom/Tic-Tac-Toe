30296ac2c14661171a25eeab22dea7076bb28a96
Tennesseefoster99 <68438949+Tennesseefoster99@users.noreply.github.com>
2021-10-06 23:34:59 -0400

    day 2 made the board

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -7,3 +7,20 @@
 # can enter player 1 and player 2 name
 # X and O maybe other symbols like custom symbols for each player if it's two player
 # 
+board = [" " for amount_of_spots in range(1, 9 + 1)]
+def Tic_Tac_Toe_Board():
+    print("\n")
+    print("       |           | ")
+    print("   " + board[1] + "   |     " + board[2] + "     |   " +  board[3])
+    print("_______|___________|_______")
+    print("       |           |")
+    print("       |           |")
+    print("_______|___________|_______")
+    print("       |           | ")
+    print("       |           | ")
+    print("       |           | ")
+
+    
+
+
+print(Tic_Tac_Toe_Board())
