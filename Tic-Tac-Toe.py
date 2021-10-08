c0e14ec8bbe9dd6db411bcfece5a2c440f27615f
Tennesseefoster99 <68438949+Tennesseefoster99@users.noreply.github.com>
2021-10-08 17:04:04 -0400

    Day 2 tic-tac-toe winner

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -7,20 +7,33 @@
 # can enter player 1 and player 2 name
 # X and O maybe other symbols like custom symbols for each player if it's two player
 # 
-board = [" " for amount_of_spots in range(1, 9 + 1)]
+board = [" " for amount_of_spots in range(10)]
 def Tic_Tac_Toe_Board():
     print("\n")
     print("       |           | ")
     print("   " + board[1] + "   |     " + board[2] + "     |   " +  board[3])
     print("_______|___________|_______")
     print("       |           |")
-    print("       |           |")
+    print("   " + board[4] + "   |     " + board[5] + "     |   " +  board[6])
     print("_______|___________|_______")
     print("       |           | ")
+    print("   " + board[7] + "   |     " + board[8] + "     |   " +  board[9])
     print("       |           | ")
-    print("       |           | ")
 
-    
+def LetterInBox(Letter, Position ): # to place the letter on the board
+    board[Letter] = Position
 
+def IsTheBoxOpen(Position): # can you place a letter  
+    return board[Position] == " "
 
-print(Tic_Tac_Toe_Board())
+def Winner(boardSpot, Letter): # how to win the game
+    return (boardSpot[1] == Letter and boardSpot[2] == Letter and boardSpot[3] == Letter) or (
+        boardSpot[4] == Letter and boardSpot[5] == Letter and boardSpot[6] == Letter
+    ) or (
+        boardSpot[7] == Letter and boardSpot[8] == Letter and boardSpot[9] == Letter
+    ) or (boardSpot[1] == Letter and boardSpot[4] == Letter and boardSpot[7] == Letter) or(
+        boardSpot[2] == Letter and boardSpot[5] == Letter and boardSpot[8] == Letter) or (
+            boardSpot[3] == Letter and boardSpot[6] == Letter and boardSpot[9] == Letter) or (
+                boardSpot[1] == Letter and boardSpot[5] == Letter and boardSpot[9] == Letter) or (
+                    boardSpot[3] == Letter and boardSpot[5] == Letter and boardSpot[7] == Letter)
+
