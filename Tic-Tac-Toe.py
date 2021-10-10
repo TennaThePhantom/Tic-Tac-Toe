3a72041a81457e20c7a33f77e48b68c0df5562d6
Tennesseefoster99 <68438949+Tennesseefoster99@users.noreply.github.com>
2021-10-09 21:53:54 -0400

    Day 3 started ai and two player

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -7,7 +7,10 @@
 # can enter player 1 and player 2 name
 # X and O maybe other symbols like custom symbols for each player if it's two player
 # 
+import random
+
 board = [" " for amount_of_spots in range(10)]
+List_of_ai_names = ["Jerry", "Jackson", "Mike", "John", "Peter", "Stella", "Anna", "Mia"]
 def Tic_Tac_Toe_Board():
     print("\n")
     print("       |           | ")
@@ -37,3 +40,56 @@ def Winner(boardSpot, Letter): # how to win the game
                 boardSpot[1] == Letter and boardSpot[5] == Letter and boardSpot[9] == Letter) or (
                     boardSpot[3] == Letter and boardSpot[5] == Letter and boardSpot[7] == Letter)
 
+def IstheBoardFull(board):
+    return ""
+
+
+def randomMove():
+    return ""
+
+def playerMove():
+    return ""
+
+def player2Move():
+    return ""
+
+def AiMove():
+    return ""
+
+
+print("Welcome to Tic Tac Toe!")
+Tic_tac_toe_has_Started = True
+while Tic_tac_toe_has_Started == True:
+    Ai = False
+    Two_player = False
+    Choose_between_2_player_or_Ai = input("Do you want to vs the Ai or Two player? ").lower()
+    if Choose_between_2_player_or_Ai == "ai":
+        Ai = True
+        Two_player = False
+        break
+    elif Choose_between_2_player_or_Ai == "two player":
+        Ai = False
+        Two_player = True
+        break
+    else:
+        print("I don't understand what you said. Please say ai or two player ")
+    continue
+
+if Two_player == True and Ai == False:
+    player1 = input("Player 1 enter your name ").upper()
+    player2 = input("player 2 enter your name ").upper()
+    print("Hello " + player1 + " and " + player2)
+    print(Tic_Tac_Toe_Board())
+
+
+
+if Two_player == False and Ai == True:
+    player1 = input("Enter your name ").upper()
+    Ai_name = random.choice(List_of_ai_names)
+    print(Ai_name)
+    print(Tic_Tac_Toe_Board())
+    while Two_player == False and Ai == True:
+        while not (IstheBoardFull(board)):
+ 
+            if IstheBoardFull(board):
+                print("Tie Game ")
