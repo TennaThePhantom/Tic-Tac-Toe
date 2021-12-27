82c4b5c0f998bbc41a0f2e15f8f030cf2f07f382
Tennessee Foster <68438949+TennaThePhantom@users.noreply.github.com>
2021-12-27 17:57:26 -0500

    variables changes, and other fixes

--- Board3by3ai.py
+++ Board3by3ai.py
@@ -6,23 +6,23 @@ from pygame.locals import *
 
 pygame.init()
 
-# the board
-Board_Height = 450
-Board_Width = 450
+# the board dimensions 
+Ai_Board3X3_Height = 450
+Ai_Board3X3_Width = 450
 Lines_Width = 15
 
-Board_Screen = pygame.display.set_mode((Board_Width, Board_Height))
-pygame.display.set_caption('Ai Tic Tac Toe 3X3 Board')
+Board_Screen = pygame.display.set_mode((Ai_Board3X3_Width, Ai_Board3X3_Height))
+pygame.display.set_caption('Ai Tic-Tac-Toe 3X3 Board')
 
 # colors for board
 ROSE = (204, 0, 204)
 ORANGE = (255, 153, 51)
 DARK_BLUE = (0, 0, 204)
 WHITE = (255, 255, 255)
-
+BLACK = (0, 0, 0)
 
 # define font
-font = pygame.font.SysFont(None, 40)
+Font = pygame.font.SysFont(None, 40)
 
 # variables for game
 Mouse_clicked = False
@@ -34,19 +34,19 @@ Game_over = False
 Winner = 0
 
 # draws a rectangle around play again 
-Play_Again_Box = Rect(Board_Width // 2 - 80, Board_Height // 2, 160, 50)
-
+Play_Again_Box = Rect(Ai_Board3X3_Width // 2 - 80, Ai_Board3X3_Height // 2, 160, 50)
 
 
 # makes the board
 def draw_board():
 	BackGround = WHITE
-	grid_color = (0, 0, 0)
+	Grid_Lines_Color = BLACK
 	Board_Screen.fill(BackGround)
 	for Grid_Lines in range(1,3):
 		"""I got 150 from doing 450 / 3 since I'm having 3 rows and column"""
-		pygame.draw.line(Board_Screen, grid_color , (0, 150 * Grid_Lines), (Board_Width,150 * Grid_Lines), Lines_Width)
-		pygame.draw.line(Board_Screen, grid_color, (150 * Grid_Lines, 0), (150 * Grid_Lines, Board_Height), Lines_Width)
+		pygame.draw.line(Board_Screen, Grid_Lines_Color , (0, 150 * Grid_Lines), (Ai_Board3X3_Width,150 * Grid_Lines), Lines_Width)
+		pygame.draw.line(Board_Screen, Grid_Lines_Color, (150 * Grid_Lines, 0), (150 * Grid_Lines, Ai_Board3X3_Height), Lines_Width)
+
 
 # makes X and O
 def draw_Letter():
@@ -69,7 +69,7 @@ def draw_Letter():
 			y_position += 1
 		x_position += 1	
 
-"""Checks the board if player 1 or ai have won or is it a tie game(is the board full)"""
+"""Checks the board if player 1 or ai have won or is it a tie game"""
 def Is_the_game_over():
 	# acesss variables outside the function
 	global Game_over
@@ -85,51 +85,51 @@ def Is_the_game_over():
 			Winner = 2
 			Game_over = True
 		# checks for 3 in a row 
-		if Board[0][Letter] + Board [1][Letter] + Board [2][Letter] == 3:
+		if Board[0][Letter] + Board[1][Letter] + Board[2][Letter] == 3:
 			Winner = 1
 			Game_over = True
-		if Board[0][Letter] + Board [1][Letter] + Board [2][Letter] == -3:
+		if Board[0][Letter] + Board[1][Letter] + Board[2][Letter] == -3:
 			Winner = 2
 			Game_over = True
 		Letter += 1
 
 	# checks for 3 in a row diagonal 
-	if Board[0][0] + Board[1][1] + Board [2][2] == 3 or Board[2][0] + Board[1][1] + Board [0][2] == 3:
+	if Board[0][0] + Board[1][1] + Board[2][2] == 3 or Board[2][0] + Board[1][1] + Board[0][2] == 3:
 		Winner = 1
 		Game_over = True
-	if Board[0][0] + Board[1][1] + Board [2][2] == -3 or Board[2][0] + Board[1][1] + Board [0][2] == -3:
+	if Board[0][0] + Board[1][1] + Board[2][2] == -3 or Board[2][0] + Board[1][1] + Board[0][2] == -3:
 		Winner = 2
 		Game_over = True
 
 	# checks for tie game
 	if Game_over == False:
-		tie = True
+		Tie = True
 		for row in Board:
 			for Zero in row:
 				if Zero == 0:
-					tie = False
-		if tie == True:
+					Tie = False
+		if Tie == True:
 			Game_over = True
 			Winner = 0
 
 
 """Makes the text when game is over or tie game"""
-def draw_game_over(winner):
+def draw_game_over_text(winner):
 
 	if winner != 0:
-		end_text = "Player " + str(winner) + " wins!"
+		End_Text = "Player " + str(winner) + " wins!"
 	elif winner == 0:
-		end_text = "You have tied!"
+		End_Text = "You have tied!"
 
-	end_img = font.render(end_text, True, DARK_BLUE)
+	End_Img = Font.render(End_Text, True, DARK_BLUE)
 	"""Either play around with draw.rect and blit for this part or do pin point calculations with your board"""
-	pygame.draw.rect(Board_Screen, ORANGE, (Board_Width // 2 - 100, Board_Height // 2 - 60, 205, 50))
-	Board_Screen.blit(end_img, (Board_Width // 2 - 100, Board_Height // 2 - 50))
+	pygame.draw.rect(Board_Screen, ORANGE, (Ai_Board3X3_Width // 2 - 100, Ai_Board3X3_Height // 2 - 60, 205, 50))
+	Board_Screen.blit(End_Img, (Ai_Board3X3_Width // 2 - 100, Ai_Board3X3_Height // 2 - 50))
 
 	Play_Again = 'Play Again?'
-	Play_Again_IMG = font.render(Play_Again, True, DARK_BLUE)
+	Play_Again_IMG = Font.render(Play_Again, True, DARK_BLUE)
 	pygame.draw.rect(Board_Screen, ORANGE, Play_Again_Box)
-	Board_Screen.blit(Play_Again_IMG, (Board_Width // 2 - 80, Board_Height // 2 + 10))
+	Board_Screen.blit(Play_Again_IMG, (Ai_Board3X3_Width // 2 - 80, Ai_Board3X3_Height // 2 + 10))
 
 
 
@@ -142,52 +142,52 @@ def computerMove(AiTurn):
 	"""Makes sure that if player 1 or player 2 wins 
 	need this since after player 1 places doesn't matter if win or not ai will place a letter after
 	if player wins make the winning move first but the ai does it counts ai as the winner that's"""
-	for row in range(3):
-		for colum in range(3):
-			board_copy = copy.deepcopy(Board) 
-			if board_copy[Random_row][Random_colum] == 0: 
-				board_copy[Random_row][Random_colum] = AiTurn
-				if Is_the_game_over() in board_copy:
+	for Row in range(3):
+		for Colum in range(3):
+			Board_Copy = copy.deepcopy(Board) 
+			if Board_Copy[Random_row][Random_colum] == 0: 
+				Board_Copy[Random_row][Random_colum] = AiTurn
+				if Is_the_game_over() in Board_Copy:
 					Ai_Board.append(draw_Letter())
-					return board_copy
+					return Board_Copy
 
 	"""Makes the Ai be able to move"""
-	for row in range(3): # three rows
-		for colum in range(3): # three columns
-			board_copy = copy.deepcopy(Board) 
-			if board_copy[Random_row][Random_colum] == 0: # chooses any location on board if it's open
-				board_copy[Random_row][Random_colum] = AiTurn # Ai turn to go
-				if Is_the_game_over() in board_copy: # is the game over in the ai board
+	for Row in range(3): # three rows
+		for Colum in range(3): # three columns
+			Board_Copy = copy.deepcopy(Board) 
+			if Board_Copy[Random_row][Random_colum] == 0: # chooses any location on board if it's open
+				Board_Copy[Random_row][Random_colum] = AiTurn # Ai turn to go
+				if Is_the_game_over() in Board_Copy: # is the game over in the ai board
 					Game_over = True
 					Winner = 2
 				else: # if not continue add a letter until game is tied, player 1 wins or the ai wins 
 					Ai_Board.append(draw_Letter())
-					board_copy[Random_row][Random_colum] = AiTurn
-					return board_copy
+					Board_Copy[Random_row][Random_colum] = AiTurn
+					return Board_Copy
 
 	"""if the board is full there is no available moves 
 	but the Ai needs to know that or else it will try to place a letter and crash the program
 	Also the Ai must know the player placed a letter in the box too if not it will try to place a letter 
 	in a box with a letter and crash the program"""
-	move = Is_the_box_open()
-	if move != None: 
-		x, y = move
-		board_copy = copy.deepcopy(Board)
-		board_copy[y][x] = AiTurn
+	Move = is_the_box_open()
+	if Move != None: 
+		x, y = Move
+		Board_Copy = copy.deepcopy(Board)
+		Board_Copy[y][x] = AiTurn
 		if AiTurn == -1:
 			Ai_Board.append(draw_Letter())
-		return board_copy
+		return Board_Copy
 	return Board
 
 
-def Is_the_box_open():
-	validMoves = [] # valid moves
-	for row in range(3): # 3 rows
-		for colum in range(3): # 3 columns
-			if Board[row][colum] == 0: # any spot with 0
-				validMoves.append((colum, row)) # adds all aviable moves for the ai to move
-	if len(validMoves) > 0: # if any spot is zero you can place a letter
-		return random.choice(validMoves)
+def is_the_box_open():
+	ValidMoves = [] # valid moves
+	for Row in range(3): # 3 rows
+		for Colum in range(3): # 3 columns
+			if Board[Row][Colum] == 0: # any spot with 0
+				ValidMoves.append((Colum, Row)) # adds all aviable moves for the ai to move
+	if len(ValidMoves) > 0: # if any spot is zero you can place a letter
+		return random.choice(ValidMoves)
 	else:
 		return None	# nothing available 
 
@@ -214,7 +214,6 @@ def start_Ai_3by3_Board():
 		for window in pygame.event.get():
 			if window.type == pygame.QUIT:
 				Start_Tic_Tac_Toe = False
-				
 			"""for player 1 and player2 mouse clicks to be register on the board"""
 			if Game_over == False:
 				if window.type == pygame.MOUSEBUTTONDOWN and Mouse_clicked == False:
@@ -230,10 +229,9 @@ def start_Ai_3by3_Board():
 						if Player == 1: # for player 1 
 							Is_the_game_over() # checks game over
 							print(Board)
-							"""if the game is over for player 1 it tells the Ai it can't place a letter 
-							after player 1 wins
-							take away the if else statement 
-							the Ai places a letter after the player 1 wins """ 
+							"""If the game is over for player 1 it tells the Ai it can't place a letter 
+							after player 1 wins take away the if else statement 
+							the Ai places a letter after player 1 wins """ 
 							if Game_over == True: 
 								Winner = 1
 							else:
@@ -243,11 +241,10 @@ def start_Ai_3by3_Board():
 							Is_the_game_over()
 							print(Board)
 							Player *= -1 # swap back to player 1
-
 	
 		# if someone has won the game or tied
 		if Game_over == True:
-			draw_game_over(Winner)
+			draw_game_over_text(Winner)
 			# check for if we clicked on Play Again
 			if window.type == pygame.MOUSEBUTTONDOWN and Mouse_clicked == False:
 				Mouse_clicked = True
@@ -261,12 +258,11 @@ def start_Ai_3by3_Board():
 					Position = (0,0)
 					Board = []
 					Winner = 0
-					# creates empty 3 x 3 board again
+					# creates empty 3 x 3 board again and rests ai board
 					Board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 					Ai_Board = []
 		# update display
 		pygame.display.update()
-	
 	pygame.quit()
 
 
